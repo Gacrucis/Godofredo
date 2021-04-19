57cb5c0c6c7b7be74705cdc0e2293a1f09038c91
VashLT <mcvicksilvaa@gmail.com>
2021-04-19 12:08:33 -0500

    added TimeLine enhanced preset

--- presets.py
+++ presets.py
@@ -2,6 +2,170 @@ import math
 from itertools import cycle
 from manim import *
 
+
+class TimeLine(VGroup):
+    def __init__(
+        self, times: list, length: float = None, time_buff: float = None, time_scale: float = None,
+        dot_scale: float = None, dot_colors: list = None, arrow_buff: float = None,
+        arrow_scale: float = None, line_config: dict = {}, arrow_config: dict = {},
+        time_config: dict = {}, dot_config: dict = {}, *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+
+        self.length = length or Camera(None).frame_width / 2
+        self.size = len(times)
+
+        # control next time index and next dot index
+        self.next = [0, 0]
+        
+
+        height_buff = 1.3
+        frame_height = Camera(None).frame_height
+
+        left_x_coord = LEFT * self.length / 2
+        left_y_coord = UP * (frame_height / 2 - height_buff)
+
+        self.initial_point = left_x_coord + left_y_coord
+
+        # horizontally centered
+        self.end_point = -left_x_coord + left_y_coord
+
+        # create main line
+
+        self.line = Line(
+            start=self.initial_point,
+            end=self.end_point,
+            **line_config
+        )
+        self.dots = VGroup()
+        self.times = VGroup()
+
+        dot_scale = dot_scale or 1
+        time_scale = time_scale or 0.6
+        arrow_scale = arrow_scale or 1
+
+        time_buff = time_buff or 0.5
+        self.arrow_buff = arrow_buff = arrow_buff or 0.3
+
+        # default colors
+
+        default_dots_color = BLUE
+        default_arrow_color = WHITE
+
+        if not dot_colors:
+            dot_colors = [default_dots_color] * self.size
+
+        point_distance = 1 / (self.size - 1)
+
+        # create dots and times
+
+        for n, time in enumerate(times):
+            position = self.line.point_from_proportion(point_distance * n)
+
+            dot = Dot(**dot_config).scale(dot_scale).move_to(position)
+
+            dot.set_color(dot_colors[n])
+
+            self.dots.add(dot)
+
+            time_mob = Tex(time, **time_config).scale(time_scale)
+
+            time_mob.next_to(position, DOWN, time_buff)
+
+            self.times.add(time_mob)
+
+        # create arrow
+        initial_dot_center = self.dots[0].get_center()
+        self.arrow = Arrow(
+            start=ORIGIN,
+            end=DOWN,
+            **arrow_config
+        ).scale(arrow_scale)
+
+        # predefine buff for next_time method works properly
+        self.arrow.next_to(self.dots[0], UP, buff=self.arrow_buff)
+
+        if not "color" in arrow_config:
+            self.arrow.set_color(default_arrow_color)
+
+        self.add(
+            self.line,
+            *self.dots,
+            *self.times
+        )
+
+    def get_points(self) -> VGroup:
+        return self.dots
+
+    def get_times(self) -> VGroup:
+        return self.times
+
+    def get_line(self) -> Line:
+        return self.line
+
+    def get_next_time(self) -> Tex:
+        assert hasattr(self, "next")
+        if self.next[0] > self.size:
+            raise Exception("Next time exceeds size")
+
+        next_time = self.times[self.next[0]]
+        self.next[0] += 1
+
+        return next_time
+
+    def get_next_dot(self) -> Dot:
+        assert hasattr(self, "next")
+        if self.next[1] > self.size:
+            raise Exception("Next dot exceeds size")
+
+        next_dot = self.dots[self.next[1]]
+        self.next[1] += 1
+
+        return next_dot
+
+    def create(self, with_arrow: bool = False, with_time: bool = False) -> AnimationGroup:
+        animations = [
+            DrawBorderThenFill(self.line),
+            DrawBorderThenFill(self.dots)
+        ]
+        if with_arrow:
+            animations.append(
+                DrawBorderThenFill(self.arrow)
+            )
+
+        if with_time:
+            self.next[0] += 1 # increased next time index
+            animations.append(
+                Write(self.times[0])
+            )
+        
+        self.next[1] += 1 # increases next dot index
+        return AnimationGroup(*animations)
+
+    def next_time(self) -> AnimationGroup:
+        """
+            animate both arrow shift and time write
+        """
+        target_time = self.get_next_time()
+        next_dot = self.get_next_dot()
+
+        animations = [
+            self.arrow.animate.next_to(next_dot, UP, buff=self.arrow_buff),
+            Write(target_time)
+        ]
+
+        return AnimationGroup(*animations)
+
+    def fade(self) -> AnimationGroup:
+        animations = [
+            FadeOut(self.dots),
+            FadeOut(self.line),
+            FadeOut(self.arrow),
+            FadeOut(self.times)
+        ]
+        return AnimationGroup(*animations)
+
+
 def get_vmobjects_from_scene(scene):
 
     mobjects = scene.mobjects
@@ -13,38 +177,40 @@ def get_vmobjects_from_scene(scene):
 
     return vmobjects
 
+
 def create_timeline(dot_amount=5, dot_scale=1, lenght=Camera(None).frame_width-4, height_buff=1.3, dot_colors=None, timeline_color=WHITE):
 
-        initial_point = Point()
-        initial_point.align_on_border(UP, buff=height_buff)
-        initial_point.shift(LEFT*lenght/2)
+    initial_point = Point()
+    initial_point.align_on_border(UP, buff=height_buff)
+    initial_point.shift(LEFT*lenght/2)
 
-        timeline_line = Line(start=initial_point, end=initial_point.get_center()+RIGHT*lenght)
+    timeline_line = Line(start=initial_point,
+                         end=initial_point.get_center()+RIGHT*lenght)
 
-        point_distance = 1/(dot_amount-1)
+    point_distance = 1/(dot_amount-1)
 
-        dots = VGroup()
+    dots = VGroup()
 
-        for n in range(dot_amount): 
+    for n in range(dot_amount):
 
-            current_dot = Dot()
-            current_dot.scale(dot_scale)
-            if dot_colors:
-                current_dot.set_color(dot_colors[n])
+        current_dot = Dot()
+        current_dot.scale(dot_scale)
+        if dot_colors:
+            current_dot.set_color(dot_colors[n])
 
-            dot_position = timeline_line.point_from_proportion(point_distance*n)
-            current_dot.move_to(dot_position)
+        dot_position = timeline_line.point_from_proportion(point_distance*n)
+        current_dot.move_to(dot_position)
 
-            dots.add(current_dot)
-        
-        return VGroup(timeline_line, dots)
+        dots.add(current_dot)
+
+    return VGroup(timeline_line, dots)
 
 
 def create_bullet_point(text, text_color=WHITE, bullet_scale=0.8, bullet_color=None, bullet_buff=0.2):
 
     if bullet_color is None:
         bullet_color = text_color
-    
+
     bullet = Dot(color=bullet_color)
     bullet.scale(bullet_scale)
 
@@ -66,27 +232,28 @@ def create_bullet_list(text_list, start_point=ORIGIN, text_colors=None, bullet_s
 
     if text_colors is None:
         text_colors = [WHITE, WHITE]
-    
+
     if bullet_colors is None:
         bullet_colors = text_colors
-    
+
     text_colors = cycle(text_colors)
     bullet_colors = cycle(bullet_colors)
 
     for i, text in enumerate(text_list):
-        text_bullet = create_bullet_point(text, text_color=next(text_colors), bullet_scale=bullet_scale, bullet_color=next(bullet_colors), bullet_buff=bullet_buff)
+        text_bullet = create_bullet_point(text, text_color=next(
+            text_colors), bullet_scale=bullet_scale, bullet_color=next(bullet_colors), bullet_buff=bullet_buff)
         text_bullet.scale(text_scale)
         text_bullet[0].move_to(start_point)
         text_bullet[0].align_to(start_point, UP)
 
         bullet_list.add(text_bullet)
-    
+
     for i, text_bullet in enumerate(bullet_list):
 
         if i:
             text_bullet[0].next_to(bullet_list[i-1][0], DOWN, buff=text_buff)
             text_bullet[0].align_to(bullet_list[i-1][0], LEFT)
-            
+
     return bullet_list
 
 
@@ -117,21 +284,19 @@ def create_chapter(title, subtitle, color=MAROON_C, subtitle_buff=-0.5, scale_fa
 
     return [anims, VGroup(title, subtitle)]
 
-
-    
     # title = Tex(text, color=color)
     # title.scale(scale_factor)
 
     # bar = Line()
     # bar.set_opacity(0)
-    
+
     # bar_radius = Camera().frame_width/2 - bar_buff
 
     # if bar:
 
     #     bar = Line(start=LEFT*bar_radius, end=RIGHT*bar_radius, color=bar_color)
     #     bar.next_to(title, DOWN, buff=0.2)
-    
+
     # return VGroup(title, bar)
 
 
@@ -163,31 +328,31 @@ def create_ripple(center=ORIGIN, max_radius=3, color=WHITE, current_radius=0):
 
 def get_decay_func(treshold):
 
-        # Creates a logaritmic function that equals 0 when x = 0
-        # and equals 1 when x = treshold
-        # f(x) = ln((decay_rate)x + 1)
+    # Creates a logaritmic function that equals 0 when x = 0
+    # and equals 1 when x = treshold
+    # f(x) = ln((decay_rate)x + 1)
 
-        decay_rate = (math.e - 1)/treshold
+    decay_rate = (math.e - 1)/treshold
 
-        def decay_func(x):
-            return math.log(decay_rate * x + 1)
-        
-        return decay_func
+    def decay_func(x):
+        return math.log(decay_rate * x + 1)
+
+    return decay_func
 
 
 def create_interference_pattern(amount=5, width=0.5, height=1.5):
 
-        pattern = VGroup()
-        pattern_color = YELLOW_B
+    pattern = VGroup()
+    pattern_color = YELLOW_B
 
-        for n in range(amount):
+    for n in range(amount):
 
-            light = Rectangle(height=height, width=width)
-            light.set_fill(color=pattern_color, opacity=0.8)
-            light.set_stroke(color=YELLOW_A, width=4)
+        light = Rectangle(height=height, width=width)
+        light.set_fill(color=pattern_color, opacity=0.8)
+        light.set_stroke(color=YELLOW_A, width=4)
 
-            light.shift(RIGHT*width*n*2)
-            
-            pattern.add(light)
-        
-        return pattern
+        light.shift(RIGHT*width*n*2)
+
+        pattern.add(light)
+
+    return pattern
